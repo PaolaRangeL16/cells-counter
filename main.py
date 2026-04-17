c6d01a9f02feff482556a148bd7a93fdfe62fdfb
Paola Rangel L <ibpaolarangel16@gmail.com>
2026-04-17 14:23:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from cell_counter.preprocessing import to_gray, gaussian_filter
+from cell_counter.thresholding import otsu_threshold
+from cell_counter.segmentation import connected_components
+from cell_counter.counting import count_objects
 
+print("Estructura funcionando correctamente")
