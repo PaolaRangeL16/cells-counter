bb0b8ef0e48ea873a1a80159d12115a33ca3cd9f
vickydiazdeleon225-sketch <vickydiazdeleon225@gmail.com>
2026-04-20 18:47:45 -0600

    Update thresholding.py

--- cell_counter/thresholding.py
+++ cell_counter/thresholding.py
@@ -1,5 +1,18 @@
-def threshold(img, T):
-    return img
+def threshold(img, T): #Función para crear imagen Blanco y Negro 
+
+    filas, columnas = img.shape #Sacar las dimensiones 
+    
+    imgBN = np.zeros((filas, columnas), dtype=np.uint8) #Imagen nueva "vacia"
+
+    for i in range(filas): #Llenado de la imagen
+        for j in range(columnas):
+
+            if img[i,j] > T: #Dependiendo del valor del pixel, se pone Blanco y Negro 
+                imgBN[i,j] = 255
+            else:
+                imgBN[i,j] = 0
+
+    return imgBN #Imagen en Blanco y Negro
 
 def otsu_threshold(img):
     return img
