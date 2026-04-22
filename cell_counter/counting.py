ec956ae64e67a7fdf01736c118527074fa3748b6
Paola Rangel L <ibpaolarangel16@gmail.com>
2026-04-22 10:40:27 -0600

    Update counting.py

--- cell_counter/counting.py
+++ cell_counter/counting.py
@@ -1,5 +1,16 @@
-def count_objects(labels):
-    return 0
+from .thresholding import otsu_threshold
+from .segmentation import connected_components
+from .preprocessing import to_gray
 
 def count_cells(image):
-    return 0
+
+    # 1. gris
+    gray = to_gray(image)
+
+    # 2. umbral
+    binary = otsu_threshold(gray)
+
+    # 3. segmentación
+    labeled, num_cells = connected_components(binary)
+
+    return num_cells
