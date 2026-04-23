a26fa991d59535271aabedfa993e99918dc41756
Paola Rangel L <ibpaolarangel16@gmail.com>
2026-04-23 11:47:44 -0600

    Update counting.py

--- cell_counter/counting.py
+++ cell_counter/counting.py
@@ -1,16 +1,19 @@
+from .preprocessing import preprocess_image
 from .thresholding import otsu_threshold
-from .segmentation import connected_components
-from .preprocessing import to_gray
+from .segmentation import connected_components, remove_small_regions
 
 def count_cells(image):
 
-    # 1. gris
-    gray = to_gray(image)
+    # 1. preprocessing completo
+    processed = preprocess_image(image)
 
-    # 2. umbral
-    binary = otsu_threshold(gray)
+    # 2. umbralización
+    binary = otsu_threshold(processed)
 
     # 3. segmentación
     labeled, num_cells = connected_components(binary)
+
+    # 4. limpieza de ruido (CLAVE)
+    labeled, num_cells = remove_small_regions(labeled, min_area=20)
 
     return num_cells
