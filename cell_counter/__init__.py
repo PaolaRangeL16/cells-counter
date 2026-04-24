5c2eb12a8de8223a8e432f7a6337d87f8a4449cb
Paola Rangel L <ibpaolarangel16@gmail.com>
2026-04-23 20:44:48 -0600

    Update __init__.py

--- cell_counter/__init__.py
+++ cell_counter/__init__.py
@@ -1,1 +1,14 @@
+# __init__.py
+from .counting import count_cells
+from .preprocessing import preprocess_image, to_gray, gaussian_filter, gamma_correction
+from .thresholding import threshold, otsu_threshold, adaptive_threshold
+from .segmentation import (
+    connected_components,
+    remove_small_regions,
+    visualize_labels,
+    get_cell_properties
+)
+from .morphology import erode, dilate, morphological_close, morphological_open
 
+__version__ = "1.0.0"
+__author__ = "VMP
